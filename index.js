d5e7179e8a9358b5ea37218c1e2c618e57e6f664
Gustavo <141040266+GustavoFiguerido1010@users.noreply.github.com>
2023-09-05 11:25:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function enviarDatos() {
+    // Obtener los valores de los campos del formulario
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var fechaNacimiento = document.getElementById("fecha_nacimiento").value;
+
+    // Crear un objeto JSON con los datos
+    var datos = {
+        nombre: nombre,
+        apellido: apellido,
+        fechaNacimiento: fechaNacimiento
+    };
+
+    // Realizar una solicitud POST al endpoint
+    fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        body: JSON.stringify(datos),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Respuesta del servidor:", data);
+        
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+}
